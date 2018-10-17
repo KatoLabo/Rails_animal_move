c9b22e47bdcc143f0f284a2405e9ec8260df4abd
yuki.g <yuki.goto@it.is.tohoku.ac.jp>
2018-10-17 15:04:58 +0900

    c_s_controller_modify_4th

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
-#class ApplicationController < ActionController::Base
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+#class ApplicationController < ActionController::API
 end
