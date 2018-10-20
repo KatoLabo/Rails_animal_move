195b8303aacb38d47e64d926c5c934b7a919fcda
yuki.g <yuki.goto@it.is.tohoku.ac.jp>
2018-10-21 07:57:06 +0900

    random_comment_post_2

--- db/migrate/20181020165326_add_comment_name_to_messies.rb
+++ db/migrate/20181020165326_add_comment_name_to_messies.rb
@@ -1,5 +1,5 @@
 class AddCommentNameToMessies < ActiveRecord::Migration[5.2]
   def change
-    add_column :messies, :comment_name, :text
+    add_column :messies, :comment_name, :string
   end
 end
