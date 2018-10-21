c35bc6572ad9f445d785e1b2887082332cc94615
yuki.g <yuki.goto@it.is.tohoku.ac.jp>
2018-10-21 10:50:38 +0900

    debug3

--- db/migrate/20181014081516_change_datatype_title_of_articles.rb
+++ db/migrate/20181014081516_change_datatype_title_of_articles.rb
@@ -1,8 +1,8 @@
 class ChangeDatatypeTitleOfArticles < ActiveRecord::Migration[5.2]
   def change
-    change_column :messies, :total_score, :varchar
-    change_column :messies, :mess, :varchar
-    change_column :messies, :smell, :varchar
-    change_column :messies, :user_id, :varchar
+    change_column :messies, :total_score, :string
+    change_column :messies, :mess, :string
+    change_column :messies, :smell, :string
+    change_column :messies, :user_id, :string
   end
 end
--- db/migrate/20181015054323_create_data.rb
+++
@@ -1,9 +0,0 @@
-class CreateDatas < ActiveRecord::Migration[5.2]
-  def change
-    create_table :datas do |t|
-      t.integer :value
-
-      t.timestamps
-    end
-  end
-end
